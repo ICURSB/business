25a9e2fee56e2f6c6205c3676ab975f681ddce35
wushuang <909143467@qq.com>
2018-01-25 14:49:31 +0800

    测试分支
    mian/app

--- src/main/java/com/hi/business/App.java
+++ src/main/java/com/hi/business/App.java
@@ -9,5 +9,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        say("帅哥");
+    }
+    
+    
+    
+    
+    public static void say(String a){
+    	a = "我是好人！";
+    	System.out.println(a);
     }
 }
